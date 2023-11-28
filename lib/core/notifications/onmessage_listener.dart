6253647a933a0703bed4c11945ed7926ab0f75a4
esraaabdrabo <esraa@sindion.com>
2023-11-28 22:26:19 +0200

    + isNotificationsEnabled

--- lib/core/notifications/onmessage_listener.dart
+++ lib/core/notifications/onmessage_listener.dart
@@ -1,13 +1,19 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:upgrade_traine_project/core/datasources/shared_preference.dart';
 import 'package:upgrade_traine_project/core/notifications/show.dart';
 
-void handleOnMessageListener(RemoteMessage event) {
+void handleOnMessageListener(RemoteMessage event) async {
   if (_hasChannelName(event)) {
     //this is notification from agora
     //voice call or video call
     _showAgoraNotification(event);
   } else {
-    showNotification(false, event, "${event.notification}");
+    bool? isNotificationsEnabled =
+        (await SpUtil.instance).getBool("is_notifications_enabled");
+
+    if (isNotificationsEnabled == null || isNotificationsEnabled) {
+      showNotification(false, event, "${event.notification}");
+    }
   }
 }
 
